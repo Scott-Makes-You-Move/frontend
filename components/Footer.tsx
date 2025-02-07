3124ceb5c674546660db8c0d7e5ac6c6943dc018
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-02-08 00:31:15 +0100

    :sparkles: Chore: setup layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 
-const Footer: React.FC = () => {
-  return (
-    <footer>
-      <div>Footer content goes here</div>
-    </footer>
-  );
-};
+const Footer: React.FC = () => (
+  <footer className="w-full text-center p-4 bg-white shadow-md mt-8">
+    <nav className="flex justify-center gap-6 text-gray-700">
+      <a href="/about" className="hover:underline">
+        About Us
+      </a>
+      <a href="/contact" className="hover:underline">
+        Contact
+      </a>
+    </nav>
+  </footer>
+);
 
 export default Footer;
