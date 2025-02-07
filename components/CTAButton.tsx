dfe62d3d83035e60b2a30b85183b5986d92350fd
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-02-07 23:47:34 +0100

    :sparkles: Chore: call-to-action added

---
+++ components/CTAButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface CTAButtonProps {
+  text: string;
+  onClick: () => void;
+}
+
+const CTAButton: React.FC<CTAButtonProps> = ({ text, onClick }) => (
+  <button
+    onClick={onClick}
+    className="mt-6 px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition"
+  >
+    {text}
+  </button>
+);
+
+export default CTAButton;
