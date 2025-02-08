a90b80bfecd59624c38411bf24a1b5d4c41f914c
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-02-08 21:38:29 +0100

    :art: Chore: cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning className={inter.className}>
+      <body
+        suppressHydrationWarning
+        className="flex flex-col items-center justify-center min-h-screen "
+      >
         <Navbar />
         {children}
         <Footer />
