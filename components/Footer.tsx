ca296a8051d5fa68f895ea2a3b6011013fa73638
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-02-17 20:40:24 +0100

    :lipstick: Chore: update footer styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Footer: React.FC = () => (
-  <footer className="w-full text-center p-4 bg-background shadow-md mt-auto border-t-2 border-primary">
-    <nav className="flex justify-center gap-6 text-primary font-body">
+  <footer className="w-full text-center p-4 bg-primary shadow-md mt-auto border-t-2 border-primary">
+    <nav className="flex justify-center gap-6 text-background font-body">
       <p className="text-2xl font-title">Scott Makes You Move</p>
     </nav>
   </footer>
