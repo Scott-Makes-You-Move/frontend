87069d3493d725f86ebe6bec7f9a6455d3e21ba2
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-04-06 23:13:29 +0200

    :art: fix: fallback url for keycloak realms on localhost

--- src/lib/next-auth/authOptions.ts
+++ src/lib/next-auth/authOptions.ts
@@ -2,7 +2,7 @@ import { AuthOptions, TokenSet } from 'next-auth';
 import { JWT } from 'next-auth/jwt';
 import KeycloakProvider from 'next-auth/providers/keycloak';
 
-const KEYCLOAK_ISSUER = process.env.KEYCLOAK_ISSUER ?? 'http://localhost:8080/realms/myrealm';
+const KEYCLOAK_ISSUER = process.env.KEYCLOAK_ISSUER ?? 'http://localhost:8080/realms/smym-dev';
 const KEYCLOAK_CLIENT_ID = process.env.KEYCLOAK_CLIENT_ID ?? 'myclient';
 const KEYCLOAK_CLIENT_SECRET = process.env.KEYCLOAK_CLIENT_SECRET ?? 'myclientsecret';
 const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET ?? 'somesecret';
