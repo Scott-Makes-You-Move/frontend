5692f26542bc3f829bfc3f31a660588d8f706ee7
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-04-14 23:43:16 +0200

    :children_crossing: feat: give polymorphic support across any component

---
+++ src/utils/createPolymorphicComponent.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+export type AsProp<C extends React.ElementType> = {
+  as?: C;
+};
+
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
+
+export type PolymorphicComponentProp<
+  C extends React.ElementType,
+  Props = {},
+> = React.PropsWithChildren<Props & AsProp<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+
+export function createPolymorphicComponent<DefaultElement extends React.ElementType, OwnProps = {}>(
+  render: (
+    props: PolymorphicComponentProp<DefaultElement, OwnProps>,
+    ref: React.Ref<any>,
+  ) => React.ReactElement | null,
+) {
+  return React.forwardRef(render) as <C extends React.ElementType = DefaultElement>(
+    props: PolymorphicComponentProp<C, OwnProps> & { ref?: React.Ref<any> },
+  ) => React.ReactElement;
+}
