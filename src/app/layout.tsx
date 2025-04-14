f6c7283e3f52677f6fba662481a3406402b5c010
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-04-14 23:11:57 +0200

    :lipstick: feat: keep footer at bottom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className="antialiased flex min-h-screen flex-col">
         <Providers>
           <SessionGuard>
             <Navbar />
