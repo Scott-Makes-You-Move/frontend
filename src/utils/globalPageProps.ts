7e31d03547306f7952e3bf8f1f1e90584a6e87c3
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-07-09 23:43:49 +0200

    :pencil2: Moving comments to highest point of the page

--- src/utils/globalPageProps.ts
+++ src/utils/globalPageProps.ts
@@ -1,6 +1,6 @@
-import type { SiteLocale } from '@/graphql/types/graphql';
-
 // TODO: Uncomment this when you need to use global page props with locale. This is currently not used in the app, so it's commented out to avoid confusion. It can be uncommented when needed for locale-based routing or other purposes.
+// import type { SiteLocale } from '@/graphql/types/graphql';
+
 /* export type GlobalPageProps = {
   params: {
     locale: SiteLocale;
