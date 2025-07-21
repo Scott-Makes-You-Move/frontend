ee330d3ca452d160271b1c214ac66a48d2d2335b
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-07-22 00:31:34 +0200

    :sparkles: Feat: read faq data from the CMS

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,10 +1,59 @@
 import requireAuth from '@/lib/auth/requireAuth';
+import { executeQuery } from '@/lib/datocms/executeQuery';
+import { graphql } from '@/lib/datocms/graphql';
+import { draftMode } from 'next/headers';
 import FAQ from './faq';
+
+type PageQueryResult = {
+  page: {
+    id: string;
+    sections: Array<{
+      __typename: 'FaqSectionRecord';
+      id: string;
+      title: string;
+      questions: Array<{
+        id: string;
+        question: string;
+        answer: { value: any };
+      }>;
+    }>;
+  };
+};
+
+const query = graphql<string, never>(`
+  query MyQuery {
+    page(filter: { slug: { eq: "faq" } }) {
+      id
+      sections {
+        __typename
+        ... on FaqSectionRecord {
+          id
+          title
+          questions {
+            id
+            question
+            answer {
+              value
+            }
+          }
+        }
+      }
+    }
+  }
+`);
+
+export const revalidate = 60;
 
 const faqPage = async () => {
   await requireAuth({ callbackUrl: '/faq' });
+  const { isEnabled: isDraftModeEnabled } = await draftMode();
+  const {
+    page: { sections },
+  } = await executeQuery<PageQueryResult, never>(query, {
+    includeDrafts: isDraftModeEnabled,
+  });
 
-  return <FAQ />;
+  return <FAQ sections={sections} />;
 };
 
 export default faqPage;
