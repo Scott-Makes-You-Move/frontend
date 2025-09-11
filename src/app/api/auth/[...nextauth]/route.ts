d11acd276ad1d18864f06c76c29b76c6b8c2dcd1
seanderoo <sean.roo@koop.overheid.nl>
2025-09-11 17:54:32 +0200

    route to keycloak directly

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,22 @@
 import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/next-auth/authOptions';
 
-const handler = NextAuth(authOptions);
+import KeycloakProvider from "next-auth/providers/keycloak";
 
-export { handler as GET, handler as POST };
+const KEYCLOAK_ISSUER = process.env.KEYCLOAK_ISSUER ?? 'http://localhost:8080/realms/smym-dev';
+const KEYCLOAK_CLIENT_ID = process.env.KEYCLOAK_CLIENT_ID ?? 'myclient';
+const KEYCLOAK_CLIENT_SECRET = process.env.KEYCLOAK_CLIENT_SECRET ?? 'myclientsecret';
+
+export const authOptions = {
+  providers: [
+    KeycloakProvider({
+      clientId: KEYCLOAK_CLIENT_ID,
+      clientSecret: KEYCLOAK_CLIENT_SECRET,
+      issuer: KEYCLOAK_ISSUER,
+    }),
+  ],
+  pages: {
+    signIn: '/api/auth/signin/keycloak', // 👈 skip provider list
+  },
+};
+
+export default NextAuth(authOptions);
